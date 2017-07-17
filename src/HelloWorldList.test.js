26cd34ea3fe1ae7922794643baf17ed9ecdc1181
Ben Blackburn <benjamin.blackburn@gmail.com>
2017-07-17 17:33:43 -0500

    finish test suite

--- src/HelloWorldList.test.js
+++ src/HelloWorldList.test.js
@@ -11,5 +11,36 @@ describe(HelloWorldList, () => {
     <HelloWorldList/>
   );
 
+  it('renders a matching snapshot', () => {
+    const component = renderer.create(
+      <HelloWorldList/>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
+  it('contains AddGreeter subcomponent', () => {
+    expect(component.find(AddGreeter)).toHaveLength(1);
+  });
+
+  it('contains the same number of HelloWorld components as greetings', () => {
+    const helloWorlds = component.find(HelloWorld).length;
+    const greetings = component.state('greetings').length;
+    expect(helloWorlds).toEqual(greetings);
+  });
+
+  it('adds another greeting when add greeting function is called', () => {
+    const before = component.find(HelloWorld).length;
+    component.instance().addGreeting('Sample');
+    const after = component.find(HelloWorld).length;
+    expect(after).toBeGreaterThan(before);
+  });
+
+  it('removes a greeting from the list', () => {
+    const before = component.find(HelloWorld).length;
+    const getRidOfMe = component.state('greetings')[0];
+    component.instance().removeGreeting(getRidOfMe);
+    const after = component.find(HelloWorld).length;
+    expect(after).toBeLessThan(before);
+  });
 });
