7fa6f9f4a1e9255d6fc928306b6a24d34b13ec82
Ben Blackburn <benjamin.blackburn@gmail.com>
2017-07-17 14:04:28 -0500

    dynamically add greetings

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
 
 
 class HelloWorldList extends Component {
   constructor(props) {
     super(props);
     this.state = { greetings: ['Catherine', 'Ben', 'Patrick'] };
+    this.addGreeting = this.addGreeting.bind(this);
+  }
+
+  addGreeting(newName) {
+    this.setState({ greetings: [...this.state.greetings, newName] });
   }
 
   render() {
     return (
       <div className="HelloWorldList">
+        <AddGreeter addGreeting={this.addGreeting} />
         {this.renderGreetings()}
       </div>
     );
