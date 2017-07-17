a640b21e53c7670e1f7df1f3e5817e741d6c4708
Ben Blackburn <benjamin.blackburn@gmail.com>
2017-07-17 13:21:54 -0500

    create frenchify method and bind to 'this' in constructor

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -5,12 +5,21 @@ class HelloWorld extends Component {
   constructor(props) {
     super(props);
     this.state = { greeting: 'Hello' };
+    this.frenchify = this.frenchify.bind(this);
   }
 
   render() {
     return(
-      <div className="HelloWorld">{this.state.greeting} {this.props.name}!</div>
+      <div className="HelloWorld">
+        <p>{this.state.greeting} {this.props.name}!</p>
+        <br/>
+        <button onClick={this.frenchify}>Frenchify!</button>
+      </div>
     );
+  }
+
+  frenchify() {
+    this.setState({ greeting: 'Bonjour' });
   }
 }
 
